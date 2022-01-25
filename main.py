0f9f2452c2ab0878ac33cf32302cd963458afcc3
MarlonMartins <marlonmartinsa7x@outlook.com>
2022-01-25 11:11:57 -0300

    Create fila base

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from fila_prioritaria import FilaPrioritaria
+from fila_normal import FilaNormal
 
-# fila_teste = FilaNormal()
-# fila_teste.atualiza_fila()
-# fila_teste.atualiza_fila()
-# fila_teste.atualiza_fila()
-# print(fila_teste.chama_cliente(5))
-# print(fila_teste.chama_cliente(6))
+fila_teste = FilaNormal()
+fila_teste.atualiza_fila()
+fila_teste.atualiza_fila()
+fila_teste.atualiza_fila()
+print(fila_teste.chama_cliente(5))
+print(fila_teste.chama_cliente(6))
 
 fila_teste_2 = FilaPrioritaria()
 fila_teste_2.atualiza_fila()
